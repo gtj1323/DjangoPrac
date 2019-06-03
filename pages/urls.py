02d1edbff080c7cc8326a21e995899b8012d8d84
JBT <gtj1323@naver.com>
2019-06-03 17:00:37 +0900

    commit 2nd Django

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [ # 아래로 내려가면서 작성함.
     path('introduce/<name>/<age>/', views.introduce),
     path('times/<int:num1>/<int:num2>/', views.times),
     path('area/<int:radius>/', views.area),
+    path('dtl_example/', views.dtl_example),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
+    path('artii/', views.artii),
+    path('result/', views.result),
+    path('user-new/', views.user_new),
+    path('user-create/', views.user_create),
+    path('static-example', views.static_example)
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from datetime import datetime
+import requests
 import random
 
 # Create your views here.
@@ -42,3 +44,67 @@ def area(request, radius):
         'area': area,
     }
     return render(request, 'area.html', context)
+
+def dtl_example(requset):
+    menus=['짜장명', '탕수육', '짬뽕', '양장피']
+    my_sentence = 'Life is short, You need python'
+    messages = ['apple', 'banana', 'cucumbr', 'mango']
+    datetimenow = datetime.now()
+    empty_list = []
+    context = {
+        'menus': menus,
+        'my_sentence': my_sentence,
+        'messages': messages,
+        'datetimenow': datetimenow,
+        'empty_list': empty_list,
+    }
+    return render(requset, 'dtl_example.html', context)
+
+# Throw Catch (variable louting을 하지 않음.)
+def throw(requset):
+    return render(requset, 'throw.html')
+
+def catch(requset):
+    print(requset.GET)
+    message = requset.GET.get('message')
+    context={'message': message}
+    return render(requset, 'catch.html', context)
+
+# requsets, random 필요.
+#ASCII ART
+def artii(requset):
+    return render(requset, 'artii.html')
+def result(requset):
+    # 1. form에서 날아온 데이터를 받느다.
+    message = requset.GET.get('message')
+    print(f'message={message}')
+    # 2. http://artii.herokuapp.com/fonts_list로 요청을 보내 응답을 결과로 .text로 변환후 저장한다.
+    req = requests.get('http://artii.herokuapp.com/fonts_list').text
+    # 3. 저장한 데이터를 list 로 바꾼다.
+    fonts = req.split('\n')
+    # 4. List 안에 들어있는 요소(font) 하나를 선택해서 저장한다.
+    font = random.choice(fonts)
+    # 5. 우리가 전달한 데이터와 저장한 font를 가지고 다시 요청을 보내 해당 응답 결과를 저장한다. (.text)
+    url = f'http://artii.herokuapp.com/make?text={message}&font={font}'
+    artii_res = requests.get(url).text
+    # 5. 최종적으로 지정한 데이터를 template로 넘겨준다.
+    context = {
+        'artii': artii_res,
+        'url': url
+    }
+    return render(requset, 'result.html', context)
+
+# POST 방식
+def user_new(request):
+    return render(request, 'user_new.html')
+def user_create(request):
+    name = request.POST.get('name')
+    pwd = request.POST.get('pwd')
+    context = {
+        'name': name,
+        'pwd': pwd,
+    }
+    return render(request, 'user_create.html', context)
+
+def static_example(request):
+    return render(request, 'static_example.html')
